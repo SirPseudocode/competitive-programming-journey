a5db4380b43faddff7eb8473a560eae044ac4e34
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2026-08-16 12:29:39 +0700

    Updating solution Compfest 18 01FC

--- Compfest 18/Coder Class/01. Pemrograman Dasar/F. Rekursi/C. Hanoi Silver/solution.cpp
+++ Compfest 18/Coder Class/01. Pemrograman Dasar/F. Rekursi/C. Hanoi Silver/solution.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void hanoi(short isi, char asal, char target, char sisa){
+    if(isi > 0){
+        hanoi(isi - 1, asal, sisa, target);
+        cout << isi << ' ' << asal << ' ' << target << '\n';
+        hanoi(isi - 1, sisa, target, asal);
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    short n;
+    cin >> n;
+
+    hanoi(n, 'A', 'C', 'B');
+    return 0;
 }
