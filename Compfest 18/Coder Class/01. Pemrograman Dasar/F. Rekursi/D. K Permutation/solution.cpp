12a7afddcee3efca27b08e779387a9c59616d2bb
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2026-08-16 19:48:43 +0700

    Updating solution of Compfest 18 01FD

--- Compfest 18/Coder Class/01. Pemrograman Dasar/F. Rekursi/D. K Permutation/solution.cpp
+++ Compfest 18/Coder Class/01. Pemrograman Dasar/F. Rekursi/D. K Permutation/solution.cpp
@@ -1,9 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int N, K;
+int current_permutation[10];
+bool used[10];
 
+void FindPermutation(int current_slot) {
+    if (current_slot > N) {
+        for (int i = 1; i <= N; i++) {
+            cout << current_permutation[i];
+            if (i < N) cout << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
     
+        if (!used[i]) {
+            if (current_slot == 1 || abs(i - current_permutation[current_slot - 1]) <= K) {
+                current_permutation[current_slot] = i;
+                used[i] = true; 
+                
+                FindPermutation(current_slot + 1);
+                
+                used[i] = false; 
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> K;
+    for(int i = 0 ; i <= N ; i++) used[i] = 0;
+
+    FindPermutation(1);
+    
+    return 0;
 }
