50abd25077e6b1241d7af3e63d97f4b0ccb7d83c
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2026-08-16 13:49:37 +0700

    Merge alternative solutions of Codeforces 4A

--- Codeforces/4. Codeforces Beta Round 4 (Div. 2 Only)/4A - Watermelon/solution.cpp
+++ Codeforces/4. Codeforces Beta Round 4 (Div. 2 Only)/4A - Watermelon/solution.cpp
@@ -8,8 +8,16 @@ int main(){
     short w;
     cin >> w;
 
+// <<<<<<< ACTIVE SOLUTION (VERSION 1): Bitwise Manipulation
+
     if(w > 2 && !(w & 1)) cout << "YES" << endl;
     else cout << "NO" << endl;
 
+// ======= VERSION 2: Modulo operator
+// 
+//     if(w > 2 && w % 2 == 0) cout << "YES" << endl;
+//     else cout << "NO" << endl;
+// 
+// >>>>>>> END ALTERNATIVE SOLUTIONS
     return 0;
 }
