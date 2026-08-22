0e7193f769006432f161a07976911fc5e4a2fcfd
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2026-08-22 13:47:34 +0700

    Updating solution of GEMASTIK 18 - Penyisihan A1

--- Gemastik 18/Penyisihan/A1 Astik Berkompetisi/solution.cpp
+++ Gemastik 18/Penyisihan/A1 Astik Berkompetisi/solution.cpp
@@ -1,9 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    void solve(){
+        int n, k;
+        cin >> n >> k;
 
-    
-}
+        for(int i = 1 ; i <= n ; i++){
+            if((1 << i) > k){
+                cout << i << '\n';
+                return;
+            }
+        }
+
+        cout << n << '\n';
+        return;
+    }
+
+    int main(){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+        int t;
+        cin >> t;
+        while(t--){
+            solve();
+        }
+    }
